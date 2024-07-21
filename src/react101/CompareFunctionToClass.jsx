21ec6969ae49cd3a564dbf397eece2b2515c445f
nimely111 <samuelehnimely@gmail.com>
2024-07-21 10:30:07 +0000

    display data in the jsx

--- src/react101/CompareFunctionToClass.jsx
+++ src/react101/CompareFunctionToClass.jsx
@@ -4,13 +4,16 @@ import { useState } from 'react'
 
 
 
-function CompareFunctionToClass() {
+function CompareFunctionToClass(props) {
   const [name, setName] = useState("")
   const [age, setAge] = useState(100)
   const [isMale, setIsMale] = useState(true)
-  
+
   return (
     <div>
+      <h1>My name is {name} </h1>
+      <h2>I am {age} years old</h2>
+      <h3>I am {isMale ? "Male" : "Female"} </h3>
       <h1>Hello World from function</h1>
     </div>
   )
