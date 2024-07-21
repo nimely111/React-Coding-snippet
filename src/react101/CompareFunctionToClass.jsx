218a7bfcf0df45e8ff03f861a256c5f5cd9589d7
nimely111 <samuelehnimely@gmail.com>
2024-07-21 10:23:51 +0000

    placed all initialized states in the component and added the props in the component

--- src/react101/CompareFunctionToClass.jsx
+++ src/react101/CompareFunctionToClass.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
 
-const [name, setName] = useState("")
-const [age, setAge] = useState(100)
-const [isMale, setIsMale] = useState(true)
 
 
 
 function CompareFunctionToClass() {
+  const [name, setName] = useState("")
+  const [age, setAge] = useState(100)
+  const [isMale, setIsMale] = useState(true)
+  
   return (
     <div>
       <h1>Hello World from function</h1>
