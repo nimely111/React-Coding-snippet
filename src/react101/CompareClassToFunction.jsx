81959f7d307711e006f21a4c117070b760031e42
nimely111 <samuelehnimely@gmail.com>
2024-07-21 11:04:42 +0000

    modified both component using modern syntax

--- src/react101/CompareClassToFunction.jsx
+++ src/react101/CompareClassToFunction.jsx
@@ -12,13 +12,14 @@ import React, { Component } from 'react'
   render() {
     // using modern destructuring syntax
     const { name, age, isMale } = this.state;
+    const { emotion } = this.state;
 
     return (
       <div>
         <h1>My name is {name} </h1>
         <h2>I am {age} years old</h2>
         <h3>I am a {isMale? "Male" : "Female"} </h3>
-        <h4>I am felling {this.props.emotion}</h4>
+        <h4>I am felling {emotion}</h4>
       </div>
     )
     return (
--- src/react101/CompareFunctionToClass.jsx
+++ src/react101/CompareFunctionToClass.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 
 
-function CompareFunctionToClass(props) {
+function CompareFunctionToClass({ emotion, year, ...props }) {
   const [name, setName] = useState("John")
   const [age, setAge] = useState(100)
   const [isMale, setIsMale] = useState(true)
@@ -14,7 +14,7 @@ function CompareFunctionToClass(props) {
       <h1>My name is {name} </h1>
       <h2>I am {age} years old</h2>
       <h3>I am {isMale ? "Male" : "Female"} </h3>
-      <h4>I am felling {props.emotion}</h4>
+      <h4>I am felling {emotion}</h4>
 
     </div>
   )
