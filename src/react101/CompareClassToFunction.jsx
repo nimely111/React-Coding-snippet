ed5f8fc6135045229162a5fa0f768aa1327a5b0e
nimely111 <samuelehnimely@gmail.com>
2024-07-21 10:35:25 +0000

    remove propstypes

--- src/react101/CompareClassToFunction.jsx
+++ src/react101/CompareClassToFunction.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 
  class CompareClassToFunction extends Component {
-  static propTypes = {
-    prop: PropTypes
-  }
 
   render() {
     return (
