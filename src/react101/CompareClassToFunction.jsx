b77ed1cfd734b58776f7a347e55944c3fe92e827
nimely111 <samuelehnimely@gmail.com>
2024-07-21 10:38:45 +0000

    initialize state data with object literal within the class base component

--- src/react101/CompareClassToFunction.jsx
+++ src/react101/CompareClassToFunction.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 
  class CompareClassToFunction extends Component {
-
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      age: 100,
+      ismale: true
+    }
+  }
   render() {
     return (
       <div>
