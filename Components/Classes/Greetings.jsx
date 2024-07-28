dbb1bce50707ad999b42d696ba010b987efb9b7b
nimely111 <samuelehnimely@gmail.com>
2024-07-28 15:43:19 +0000

    update the rename props in the greetings file

--- Components/Classes/Greetings.jsx
+++ Components/Classes/Greetings.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 class Greetings extends Component{
     render(props){
-        return <h1>Hello, { this.props.name }</h1>
+        return <h1>Hello, { this.props.first_name }</h1>
     }
 }
 export default Greetings;
