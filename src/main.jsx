8abe5ffa642db14e5f92cc94b4d3a3b8413e01d5
nimely111 <samuelehnimely@gmail.com>
2024-07-29 21:19:49 +0000

    imported the App_2.jsx file in the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import App_2 from '../Components/Classes/App_2.jsx'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
