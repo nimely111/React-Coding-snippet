284ef6da8c2e2bdc0f397ae7e79f76fd3b687782
nimely111 <samuelehnimely@gmail.com>
2024-07-30 23:55:37 +0000

    create the handle click function and bind it with the this keyword

--- Components/Classes/Constructor_Props.jsx
+++ Components/Classes/Constructor_Props.jsx
@@ -3,5 +3,10 @@ class Constructor_Props extends Components{
     constructor(props){
         super(props);
         this.state = {counter: 0};
+        this.handleClick.bind(this)
+    }
+    
+    handleClick(){
+        
     }
 }
