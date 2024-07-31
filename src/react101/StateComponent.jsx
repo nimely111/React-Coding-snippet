ff8b917dc652576272174eac7aef2331a4c73477
nimely111 <samuelehnimely@gmail.com>
2024-07-31 23:32:14 +0000

    remove styling from the p element

--- src/react101/StateComponent.jsx
+++ src/react101/StateComponent.jsx
@@ -2,15 +2,39 @@ import React, {useState} from 'react'
 
 function StateComponent(){
     const [count, setCount] = useState(0);
+    const isPositive = false
+
+    // increase Count
     const increaseCount = () => {
         setCount(count + 1)
     }
+    
+    // decrease Count
+    const decreaseCount = () => {
+        setCount(count - 1)
+    }
+
     return(
         <>
             <h1>Welcome to my Counter App</h1>
-            <button>-</button>
+            <button style={{
+                marginRight: '5px',
+                padding: '7px 20px',
+                background: 'blue',
+                color: '#fff',
+                fontSize: '20px'
+            }} onClick={decreaseCount}>-</button>
+
+            <button style={{
+                marginRight: '5px',
+                padding: '7px 20px',
+                background: 'blue',
+                color: '#fff',
+                fontSize: '20px'
+            }}
+             onClick={increaseCount}>+</button>
+             
             <p>The count is { count }</p>
-            <button onClick={increaseCount}>+</button>
         </>
     )
 }
