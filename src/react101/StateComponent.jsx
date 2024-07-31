24af67600eecac70d95b6a46c1a78fdcfe0b1dc3
nimely111 <samuelehnimely@gmail.com>
2024-07-31 18:47:02 +0000

    building a simple counter app using function based component

--- src/react101/StateComponent.jsx
+++ src/react101/StateComponent.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function StateComponent(){
+    const [count, setCount] = useState(0);
+    increaseCount = () => {
+        setCount(count + 1)
+    }
     return(
         <>
-            
+            <h1>Welcome to my Counter App</h1>
+            <p>The count is { count }</p>
         </>
     )
 }
