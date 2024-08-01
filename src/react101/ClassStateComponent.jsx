90238634e5db67da330f40687ca8dffdf85957d4
nimely111 <samuelehnimely@gmail.com>
2024-08-01 07:52:13 +0000

    setup title for class based component

--- src/react101/ClassStateComponent.jsx
+++ src/react101/ClassStateComponent.jsx
@@ -8,6 +8,16 @@ class ClassStateComponent extends Component{
             count: 0
         }
     }
+
+
+    render(){
+        return(
+            <>
+                  <h1>Welcome to my Counter App</h1>
+                  <h2>Using Class Based Component</h2>
+            </>
+        )
+    }
 }
 
 export default ClassStateComponent
