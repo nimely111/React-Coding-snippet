b34ccea27aefa1304f25b90e3eedcdd37c0a831c
nimely111 <samuelehnimely@gmail.com>
2024-08-01 20:03:06 +0000

    created the function to manipulate the form

--- src/react101/Login.jsx
+++ src/react101/Login.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
  function Login() {
-    
+    const handleSubmit = (e) =>{
+        e.preventDeffault()
+        console.log(12345);
+    }
+
   return (
     <div>
       <form action="">
         <input type="email" placeholder='Enter Your Email'/>
         <input type="password" placeholder='Enter Your password'/>
-        <input type="submit" value='Submit'/>
+        <input type="submit" value='Submit' onClick={handleSubmit}/>
       </form>
     </div>
   )
