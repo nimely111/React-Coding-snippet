95e02344bcb861ab18529ca6080042360a020313
nimely111 <samuelehnimely@gmail.com>
2024-08-01 19:58:33 +0000

    imported the login component and nest it in the parent component

--- src/react101/App_101.jsx
+++ src/react101/App_101.jsx
@@ -1,11 +1,13 @@
 import StateComponent from "./StateComponent";
 import ClassStateComponent from "./ClassStateComponent";
+import Login from "./Login";
 
 function App_101(){
     return(
         <>
           <StateComponent />
           <ClassStateComponent />
+          <Login />
         </>
     )
 }
