a4355d438d856ba3fe33b2f3c66369dbc613159c
nimely111 <samuelehnimely@gmail.com>
2024-08-03 21:20:58 +0000

    nested an input element with the type of text and a button of add task in the add task div

--- src/Pedro_Exercise/App.jsx
+++ src/Pedro_Exercise/App.jsx
@@ -3,7 +3,11 @@ import './todo.css'
 function App() {
   return (
     <div className='App'>
-      <div className="addTask"></div>
+      <div className="addTask">
+        <input type="text" />
+        <button>Add Task</button>
+      </div>
+      <div className="list"></div>
     </div>
   )
 }
