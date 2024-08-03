15947588019ba1d45a0889ac5bd7648786e63e30
nimely111 <samuelehnimely@gmail.com>
2024-08-03 03:40:51 +0000

    added the name and description properties as props to the ItemDescription component

--- src/react101/props/product.jsx
+++ src/react101/props/product.jsx
@@ -4,11 +4,8 @@ import ItemDescription from './ItemDescription'
 function product({name, description, price}) {
   return (
     <div>
-      <h1>{name}</h1>
-      <h2>{description}</h2>
+      <ItemDescription name={name} description={description}/>
       <h3>${price}</h3>
-
-      <ItemDescription />
     </div>
   )
 }
