c87ea8131fc72415665df23c8bd3717823c653b4
nimely111 <samuelehnimely@gmail.com>
2024-08-03 21:11:12 +0000

    imported the app file from the Pedro_Exercise folder

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import App_2 from '../Components/Classes/App_2.jsx'
 import App_101 from './react101/App_101.jsx'
+import App from './Pedro_Exercise/App.jsx'
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,5 +13,6 @@ root.render(
     {/* <App /> */}
     {/* <App_2 /> */}
     <App_101 />
+    <App />
   </React.StrictMode>,
 )
