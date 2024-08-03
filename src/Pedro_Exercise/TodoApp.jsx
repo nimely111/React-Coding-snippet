803a83b3e7cc199aae2d0a3012b905e5bfbf0665
nimely111 <samuelehnimely@gmail.com>
2024-08-03 22:12:47 +0000

    import the useState hook and set the variable and function to mutate the variable accordingly

--- src/Pedro_Exercise/TodoApp.jsx
+++ src/Pedro_Exercise/TodoApp.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 import './todo.css'
 function TodoApp() {
+  const [todoList, setTodoList] = useState([])
   return (
     <div className='App'>
       <div className="addTask">
