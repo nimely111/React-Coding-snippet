792dce8d0178beb2dbb715ebfd97f570d7e73bc5
nimely111 <samuelehnimely@gmail.com>
2024-08-03 03:17:45 +0000

    display all the props in the page, and I also fixed the bugs within the App_101 during the importation of the Product file

--- src/react101/App_101.jsx
+++ src/react101/App_101.jsx
@@ -1,7 +1,7 @@
 import StateComponent from "./StateComponent";
 import ClassStateComponent from "./ClassStateComponent";
 import Login from "./Login";
-import product from "./props/product";
+import Product from "./props/product";
 
 function App_101(){
     return(
--- src/react101/props/product.jsx
+++ src/react101/props/product.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function product({name, description, price}) {
   return (
     <div>
-      
+      <h1>{name}</h1>
+      <h2>{description}</h2>
+      <h3>{price}</h3>
     </div>
   )
 }
