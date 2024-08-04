b7d914a51ce9a99e08ec1882ee10d8d2d4703bc1
nimely111 <samuelehnimely@gmail.com>
2024-08-04 21:01:54 +0000

    make code more elegant by placing the filter condition in the setTodoList function

--- src/Pedro_Exercise/TodoApp.jsx
+++ src/Pedro_Exercise/TodoApp.jsx
@@ -14,8 +14,7 @@ function TodoApp() {
   }
 
   const deleteTask = (taskName) =>{
-    const newTodoList = todoList.filter(task => task !== taskName)
-    setTodoList(newTodoList)
+    setTodoList(todoList.filter(task => task !== taskName))
   }
 
   return (
