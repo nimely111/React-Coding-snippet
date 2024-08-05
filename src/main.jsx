42af039cb8b8611f06c26e25e48da2f97bdf86c6
nimely111 <samuelehnimely@gmail.com>
2024-08-05 00:27:23 +0000

    uncomented the index.css file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import App_2 from '../Components/Classes/App_2.jsx'
 import App_101 from './react101/App_101.jsx'
-// import './index.css'
+import './index.css'
 import TodoApp from './Pedro_Exercise/TodoApp.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
