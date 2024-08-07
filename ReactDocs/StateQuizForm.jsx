b2f7727a9020a7b71ab30c194c8094f42660d05a
nimely111 <samuelehnimely@gmail.com>
2024-08-07 00:48:28 +0000

    initialized all states needed

--- ReactDocs/StateQuizForm.jsx
+++ ReactDocs/StateQuizForm.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 
 const StateQuizForm = () => {
+    const [answer, setAnswer] = useState('');
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState('typing')
 
     return (
         <div>
