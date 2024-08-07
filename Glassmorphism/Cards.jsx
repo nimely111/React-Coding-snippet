b73714ca6e1bece6bef506167f798ec5999eaa16
nimely111 <samuelehnimely@gmail.com>
2024-08-07 02:27:05 +0000

    removed the redundent cards from the card container

--- Glassmorphism/Cards.jsx
+++ Glassmorphism/Cards.jsx
@@ -8,14 +8,6 @@ function Cards() {
         <h2>Card 1</h2>
         <p>This is a description for card 1.</p>
       </div>
-      <div className="card">
-        <h2>Card 2</h2>
-        <p>This is a description for card 2.</p>
-      </div>
-      <div className="card">
-        <h2>Card 3</h2>
-        <p>This is a description for card 3.</p>
-      </div>
     </div>
   );
 }
