431293e558181b19e694d94c2fcc7ec4f02f4b2b
nimely111 <samuelehnimely@gmail.com>
2024-08-07 02:01:02 +0000

    complete form submission function

--- ReactDocs/StateQuizForm.jsx
+++ ReactDocs/StateQuizForm.jsx
@@ -49,4 +49,19 @@ const StateQuizForm = () => {
     )
 }
 
+function submitForm(answer) {
+    // Pretend it's hitting the network.
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let shouldError = answer.toLowerCase() !== 'lima'
+        if (shouldError) {
+          reject(new Error('Good guess but a wrong answer. Try again!'));
+        } else {
+          resolve();
+        }
+      }, 1500);
+    });
+  }
+  
+
 export default StateQuizForm;
