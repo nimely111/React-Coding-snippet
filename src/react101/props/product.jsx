b752440398b244db3bf11e399d87c77f9e260d97
nimely111 <samuelehnimely@gmail.com>
2024-08-08 22:45:51 +0000

    transitioning from BEM to css modules

--- src/react101/props/product.jsx
+++ src/react101/props/product.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ItemDescription from './ItemDescription'
-import './Product.css'
+import styles from './Product.module.css'
 
 function Product({name, description, price}) {
   return (
     <div className='product'>
       <ItemDescription name={name} description={description}/>
-      <h2 className="product__error">An error occured</h2>
+      <h2 className="error">An error occured</h2>
       <h3 className='product__price'>Price: ${price}</h3>
     </div>
   )
