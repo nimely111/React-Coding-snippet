f648f20fe278be40f338f662d0aa7bd0d76a4a04
nimely111 <samuelehnimely@gmail.com>
2024-08-08 22:53:34 +0000

    understanding css modules

--- src/react101/props/product.jsx
+++ src/react101/props/product.jsx
@@ -4,10 +4,10 @@ import styles from './Product.module.css'
 
 function Product({name, description, price}) {
   return (
-    <div className='product'>
+    <div className={styles.product}>
       <ItemDescription name={name} description={description}/>
-      <h2 className="error">An error occured</h2>
-      <h3 className='product__price'>Price: ${price}</h3>
+      <h2 className={styles.error}>An error occured</h2>
+      <h3 className={styles.price}>Price: ${price}</h3>
     </div>
   )
 }
