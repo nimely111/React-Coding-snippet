b74ba081730681462eccd7d69aa47be6bb18117f
nimely111 <samuelehnimely@gmail.com>
2024-08-11 19:59:01 +0000

    set text variable to read and write with the setText function and handle click function eextablish

--- src/Pedro_Exercise/Text.jsx
+++ src/Pedro_Exercise/Text.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react'
 
 const Text = () =>{
+    const [text, setText] = useState('');
+    
+    const handleClick = (e) => {
+        setText(e.target.value)
+    }
+
+
   return (
     <div>
       <input />
