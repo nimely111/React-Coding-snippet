9697ac02fab0776393c688431f3b4118f8d1c9fa
nimely111 <samuelehnimely@gmail.com>
2024-08-11 20:36:24 +0000

    export learn use effect

--- src/Pedro_Exercise/TodoApp.jsx
+++ src/Pedro_Exercise/TodoApp.jsx
@@ -101,4 +101,19 @@ const  TodoApp = () => {
   )
 }
 
+export const LearnUseEffect = () => {
+  const [showText, setShowText] = useState(false);
+
+return (
+  <div className='App'>
+    <button
+    onClick={setShowText(!showText)}
+    >
+      Show/Hide
+    </button>
+    {showText && <Text />}
+  </div>
+)
+}
+
 export default TodoApp
