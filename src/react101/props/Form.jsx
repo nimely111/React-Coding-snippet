972fecebfbeda8e58944c51c51d649265d235386
nimely111 <samuelehnimely@gmail.com>
2024-08-14 09:40:06 +0000

    remove the form element from the App_101 to the desire form component

--- src/react101/props/Form.jsx
+++ src/react101/props/Form.jsx
@@ -12,16 +12,20 @@ const Form = () => {
 
   return (
     <div>
-      <form>
-      <input
-        value={input}
-        onChange={e => setInput(e.target.value)} 
-        type="text" />
-        <button type='submit' onClick={addTodo}>Add Todo</button>
-        {todos.map(todo => (
-           <p>{todo}</p>
-        ))}
-      </form>
+     <form>
+            <input
+            value={input} 
+            onChange={e => setInput(e.target.value)}
+            type="text" />  
+            <button 
+            type="submit" 
+            onClick={addTodos}>
+                Add Todos
+            </button> 
+            </form> 
+          {todos.map((todo, key) => (
+            <p key={key}>{todo}</p>
+          ))}
     </div>
   )
 }
