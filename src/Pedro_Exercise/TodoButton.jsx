ad48098765eab00754512fa1e08feb8a6165bca2
nimely111 <samuelehnimely@gmail.com>
2024-08-19 23:54:24 +0000

    update the TodoButton component to  its previous state and remove all the Task rendering and placed back into the Task component

--- src/Pedro_Exercise/TodoButton.jsx
+++ src/Pedro_Exercise/TodoButton.jsx
@@ -1,25 +1,9 @@
 import React from 'react'
 
-const button = ({label, addTask, taskCompleted, deleteTask, isCompleted}) => {
+const button = ({label, addTask}) => {
   return (
     <div>
       <button onClick={addTask}>{label}</button>
-      <button onClick={taskCompleted}>Complete</button>
-      <button onClick={deleteTask}>X</button>
-
-      <div className='task' 
-       style={{backgroundColor: isCompleted ? 'green' : 'white', color: isCompleted && 'white'}}
-       >
-            <li>
-                {taskName}
-                {/* <button onClick={() => taskCompleted(id)}>Complete</button>
-                <button onClick={() => deleteTask(id)}>X</button> */}
-                <TodoButton
-                    taskCompleted={taskCompleted(id)}
-                    deleteTask={deleteTask(id)}
-                 />
-            </li>
-        </div>
     </div>
   )
 }
