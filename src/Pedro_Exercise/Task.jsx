1c764ef46dc116d3e5d9e9fab9a0f28355b0ec1a
nimely111 <samuelehnimely@gmail.com>
2024-08-19 23:52:13 +0000

    removed the TodoButton component from the Task component and undo all the previous edit and modifications

--- src/Pedro_Exercise/Task.jsx
+++ src/Pedro_Exercise/Task.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import './todo.css'
-import TodoButton from '../Pedro_Exercise/TodoButton'
 
 const Task = ({taskName, id, deleteTask, taskCompleted, isCompleted}) =>  {
   return (
-                <TodoButton
-                    taskCompleted={taskCompleted(id)}
-                    deleteTask={deleteTask(id)}
-                 />
+       <div className='task' 
+       style={{backgroundColor: isCompleted ? 'green' : 'white', color: isCompleted && 'white'}}
+       >
+            <li>
+                {taskName}
+                <button onClick={() => taskCompleted(id)}>Complete</button>
+                <button onClick={() => deleteTask(id)}>X</button>
+            </li>
+        </div>
   )
 }
 
