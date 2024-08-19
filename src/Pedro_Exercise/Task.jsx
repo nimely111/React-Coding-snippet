593be902321986cdc5965dd401e7a2adf7c12b02
nimely111 <samuelehnimely@gmail.com>
2024-08-19 23:48:35 +0000

    removed the code f rom the Task component and place them in the TodoButtom component

--- src/Pedro_Exercise/Task.jsx
+++ src/Pedro_Exercise/Task.jsx
@@ -4,19 +4,10 @@ import TodoButton from '../Pedro_Exercise/TodoButton'
 
 const Task = ({taskName, id, deleteTask, taskCompleted, isCompleted}) =>  {
   return (
-       <div className='task' 
-       style={{backgroundColor: isCompleted ? 'green' : 'white', color: isCompleted && 'white'}}
-       >
-            <li>
-                {taskName}
-                {/* <button onClick={() => taskCompleted(id)}>Complete</button>
-                <button onClick={() => deleteTask(id)}>X</button> */}
                 <TodoButton
                     taskCompleted={taskCompleted(id)}
                     deleteTask={deleteTask(id)}
                  />
-            </li>
-        </div>
   )
 }
 
--- src/Pedro_Exercise/TodoButton.jsx
+++ src/Pedro_Exercise/TodoButton.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const button = ({label, addTask, taskCompleted, deleteTask}) => {
+const button = ({label, addTask, taskCompleted, deleteTask, isCompleted}) => {
   return (
     <div>
       <button onClick={addTask}>{label}</button>
+      <button onClick={taskCompleted}>Complete</button>
+      <button onClick={deleteTask}>X</button>
+
+      <div className='task' 
+       style={{backgroundColor: isCompleted ? 'green' : 'white', color: isCompleted && 'white'}}
+       >
+            <li>
+                {taskName}
+                {/* <button onClick={() => taskCompleted(id)}>Complete</button>
+                <button onClick={() => deleteTask(id)}>X</button> */}
+                <TodoButton
+                    taskCompleted={taskCompleted(id)}
+                    deleteTask={deleteTask(id)}
+                 />
+            </li>
+        </div>
     </div>
   )
 }
