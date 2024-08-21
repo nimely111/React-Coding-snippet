b9e90f36a319a5057302012f990b30c4a6436f32
nimely111 <samuelehnimely@gmail.com>
2024-08-21 14:15:25 +0000

    creted a new directory and a file call RouterDom for students learning react-router-dom and call all the necessary items from the library and set the structures

---
+++ src/Pedro_Exercise/Router_DOM/RouterApp.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+const RouterApp = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+            <Route path='' />
+            <Route path='' />
+            <Route path='' />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default RouterApp
