9949a6460144e8206405143526306328f53c4c43
nimely111 <samuelehnimely@gmail.com>
2024-08-23 06:10:12 +0000

    imported the Menu and contact pages components

--- src/Pedro_Exercise/Router_DOM/RouterApp.jsx
+++ src/Pedro_Exercise/Router_DOM/RouterApp.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home'
+import Menu from './Pages/Menu';
+import Contact from './Contact';
 
 const RouterApp = () => {
   return (
