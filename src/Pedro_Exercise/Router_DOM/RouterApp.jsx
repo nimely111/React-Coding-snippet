d8148d5a28325162c34777583f02217141291427
nimely111 <samuelehnimely@gmail.com>
2024-08-23 06:36:45 +0000

    import the Link component from react-router-dom

--- src/Pedro_Exercise/Router_DOM/RouterApp.jsx
+++ src/Pedro_Exercise/Router_DOM/RouterApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './Pages/Home'
 import Menu from './Pages/Menu';
 import Contact from './Contact';
@@ -12,6 +12,7 @@ const RouterApp = () => {
             <Route path='/' element={<Home />} />
             <Route path='/menu' element={<Menu />}/>
             <Route path='/contact' element={<Contact />} />
+            <Route path='*' element={<h1>Oops! PAGE NOT FOUND</h1>} />
         </Routes>
       </Router>
     </div>
