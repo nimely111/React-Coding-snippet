a8fc90c8c9b2c1edb15ab8e9f58d69c19b4a7542
nimely111 <samuelehnimely@gmail.com>
2024-08-23 07:01:21 +0000

    add class for styling the nav links

--- src/Pedro_Exercise/Router_DOM/Nav/Navbar.jsx
+++ src/Pedro_Exercise/Router_DOM/Nav/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
         <Link to='/'> Home</Link>
         <Link to='/menu'> Menu</Link>
         <Link to='/contact'> Contact</Link>
