06ba14020a49718df0ef8c31975d3e1bcc0216ee
nimely111 <samuelehnimely@gmail.com>
2024-09-21 20:34:58 +0000

    import the useContext hook and the AppContext with in the profile component, while extracting the userName and the setUserName function from the context provider[

--- src/Pedro_Exercise/Router_DOM/Pages/Home.jsx
+++ src/Pedro_Exercise/Router_DOM/Pages/Home.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { useContext } from 'react'
 import { AppContext } from '../RouterApp'
 
-const Home = ({username}) => {
+const Home = () => {
   const { userName } = useContext(AppContext)
   return (
     <div>
-      <h1>This is the home page and user is { username }</h1>
+      <h1>This is the home page and user is { userName }</h1>
     </div>
   )
 }
--- src/Pedro_Exercise/Router_DOM/Pages/Profile.jsx
+++ src/Pedro_Exercise/Router_DOM/Pages/Profile.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ChangeProfile from '../Components/ChangeProfile'
+import { useContext } from 'react'
+import { AppContext } from '../RouterApp'
 
-const Profile = ({username, setUserName}) => {
+const Profile = () => {
+    const { userName, setUserName } = useContext(AppContext)
   return (
     <div>
-      <h1>Profile, user is {username}</h1>
+      <h1>Profile, user is {userName}</h1>
       <ChangeProfile setUserName={setUserName}/>
     </div>
   )
