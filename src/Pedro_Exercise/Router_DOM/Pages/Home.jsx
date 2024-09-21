b451592795313d90b3038c247076e13d3347e8e0
nimely111 <samuelehnimely@gmail.com>
2024-09-21 20:28:59 +0000

    import the useContext hook and the AppContext while extraction the state variable I want to use

--- src/Pedro_Exercise/Router_DOM/Pages/Home.jsx
+++ src/Pedro_Exercise/Router_DOM/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AppContext } from '../RouterApp'
 
 const Home = ({username}) => {
+  const { userName } = useContext(AppContext)
   return (
     <div>
       <h1>This is the home page and user is { username }</h1>
