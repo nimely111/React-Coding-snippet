1e490b6745e5145823efd4d2029703fc0dc08cf9
nimely111 <samuelehnimely@gmail.com>
2024-09-21 20:10:29 +0000

    learning about the useContext hook, and apply the context as AppContext

--- src/Pedro_Exercise/Router_DOM/RouterApp.jsx
+++ src/Pedro_Exercise/Router_DOM/RouterApp.jsx
@@ -3,22 +3,25 @@ import Home from './Pages/Home'
 import Profile from './Pages/Profile';
 import Contact from './Pages/Contact';
 import Navbar from './Components/Navbar';
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 
 const RouterApp = () => {
   const [userName, setUserName] = useState("JesTech");
+  const AppContext = createContext()
 
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-            <Route path='/' element={<Home username={userName} />} />
-            <Route path='/profile' element={<Profile username={userName} setUserName={setUserName} />}/>
-            <Route path='/contact' element={<Contact />} />
-            <Route path='*' element={<h1>Oops! PAGE NOT FOUND</h1>} />
-        </Routes>
-      </Router>
+      <AppContext.Provider value={userName}>
+        <Router>
+          <Navbar />
+          <Routes>
+              <Route path='/' element={<Home username={userName} />} />
+              <Route path='/profile' element={<Profile username={userName} setUserName={setUserName} />}/>
+              <Route path='/contact' element={<Contact />} />
+              <Route path='*' element={<h1>Oops! PAGE NOT FOUND</h1>} />
+          </Routes>
+        </Router>
+        </AppContext.Provider>
     </div>
   )
 }
